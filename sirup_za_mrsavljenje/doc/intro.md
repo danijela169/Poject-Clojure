9780ec6cc52f60771b6434d5079d4a05e41f1b3c
Danijela <Danijela@DESKTOP-OCPCB1S>
2020-08-01 02:18:15 +0200

    Prvi commit

---
+++ sirup_za_mrsavljenje/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sirup_za_mrsavljenje
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ sirup_za_mrsavljenje/test/sirup_za_mrsavljenje/core_test.clj
@@ -0,0 +1,7 @@
+(ns sirup-za-mrsavljenje.core-test
+  (:require [clojure.test :refer :all]
+            [sirup-za-mrsavljenje.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
